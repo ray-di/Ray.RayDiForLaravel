2b2da24221506b44b1cf1bfa1d147d418a43dcef
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-18 16:17:20 +0900

    Create initial files

---
+++ Ray/Module.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Ray;
+
+use Ray\Di\AbstractModule;
+
+final class Module extends AbstractModule
+{
+    protected function configure(): void
+    {
+    }
+}
