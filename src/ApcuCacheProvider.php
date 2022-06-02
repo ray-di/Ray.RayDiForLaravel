148a51326cad5f41dbfad7faaab27f4e7f4489e7
tsuchiya <tsuchiya@bengo4.com>
2022-06-02 23:54:34 +0900

    Use doctrine/cache v1

--- src/ApcuCacheProvider.php
+++ src/ApcuCacheProvider.php
@@ -4,17 +4,13 @@ declare(strict_types=1);
 
 namespace Ray\RayDiForLaravel;
 
+use Doctrine\Common\Cache\ApcuCache;
 use Doctrine\Common\Cache\CacheProvider;
-use Doctrine\Common\Cache\Psr6\DoctrineProvider;
-use Symfony\Component\Cache\Adapter\ApcuAdapter;
 
 class ApcuCacheProvider
 {
-    public static function create(string $namespace): CacheProvider
+    public static function create(): CacheProvider
     {
-        $cache = DoctrineProvider::wrap(new ApcuAdapter($namespace));
-        assert($cache instanceof CacheProvider);
-
-        return $cache;
+        return new ApcuCache();
     }
 }
